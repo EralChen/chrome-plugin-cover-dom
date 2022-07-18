2c7f88ebd0046841e5ad3f4b630e400c8f871f6a
EralChen <eralchen@foxmail.com>
2022-07-18 16:12:18 +0800

    NormalObject

--- packages/shared/types/base.ts
+++ packages/shared/types/base.ts
@@ -1,6 +1,6 @@
 export type ReturnVoid = void | Promise<void>
 export type AnyFunc = (...args: any[]) => any
-export type AnyObject = Record<string, any>
+export type NormalObject = Record<string, any>
 export type Merge<
   A extends Record<string, any>,
   B,
