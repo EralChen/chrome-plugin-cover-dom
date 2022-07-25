4eaa64dee4ca735fbbebee5f86953474cbba3614
EralChen <eralchen@foxmail.com>
2022-07-26 00:07:28 +0800

    RestFetch

---
+++ packages/shared/utils-fetch/index.ts
@@ -0,0 +1,16 @@
+// import { RestFetchRequest } from '../types'
+import { RestFetch } from './RestFetch'
+export {
+  RestFetch,
+}
+// const restFetch  = new RestFetch({
+//   baseURL: '/',
+//   setRequestInit (config) {
+//     return config
+//   },
+// })
+
+// export const request = <T>(...args: Parameters<RestFetchRequest>) => {
+//   return restFetch.request(...args) as Promise<T>
+// }
+
