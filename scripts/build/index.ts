755bbe8bf8c97a1deca1c305d9517d9a08aafeca
EralChen <eralchen@foxmail.com>
2022-07-28 14:07:32 +0800

    merge-css-to-dist

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -2,9 +2,11 @@ import { series } from 'gulp'
 import { run, taskWithName } from '@lib-env/shared'
 import clearDist from './clear-dist'
 import toDistType from './to-dist-type'
+import mergeCssToDist from './merge-css-to-dist'
 export default series(
   // 并行打包 packages 下的内容
   clearDist,
   taskWithName('buildPackages', async () => run('pnpm run --filter "./packages/**" --parallel build')),
   toDistType,
+  mergeCssToDist,
 )
---
+++ scripts/build/merge-css-to-dist.ts
@@ -0,0 +1,33 @@
+import { dest, series, src } from 'gulp'
+import { taskWithName } from '@lib-env/shared'
+import { distDir } from '@lib-env/path'
+import path from 'path'
+import { sync } from 'fast-glob'
+import concat from 'gulp-concat'
+export default series(
+  // 合并所有css到入口
+  taskWithName('buildCss', async () => {
+
+    const cssOutPath = path.resolve(distDir, './components')
+
+    const cssFiles = sync('**/*.css', {
+      cwd: cssOutPath,
+      onlyFiles: true,
+    })
+
+    src(
+      cssFiles.map(css => path.resolve(cssOutPath, css)),
+    )
+      .pipe(
+        concat('index.css'),
+      )
+      .pipe(
+        dest(
+          path.resolve(distDir),
+        ),
+      )
+    
+  }),
+  
+
+)
