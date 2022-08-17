8574a07a6b05ac6fda884b5b660aca844fa5c050
EralChen <eralchen@foxmail.com>
2022-08-17 10:25:40 +0800

    clear

--- docs/examples/shared-utils-fetch-rest-fetch/basic.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { ref } from 'vue'
-import { getTestJson } from './api'
-const name = ref('')
-getTestJson().then(res => name.value = res)
-</script>
-<template>
-  <div>{{ name }}</div>
-</template>
--- docs/examples/shared-utils-fetch-rest-fetch/request.ts
+++
@@ -1,9 +0,0 @@
-import type { RestFetchRequest } from '@vunk/core'
-import { RestFetch } from '@vunk-skzz/shared/utils-fetch'
-
-const restFetch  = new RestFetch({
-  baseURL: '/data',
-})
-export const request = <T>(...args: Parameters<RestFetchRequest>) => {
-  return restFetch.request(...args) as Promise<T>
-}
